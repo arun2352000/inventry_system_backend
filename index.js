bd8c0a0f57e4d1a98d9841111b7df98ad2c77ab0
arun2352000 <poorvaja10@gmail.com>
2024-08-26 10:08:48 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const PORT =process.env.PORT
 const app = express();
-const PORT =4000;
-
-// app.use()
+app.use(cors())
+app.use(express.json())
 
 app. get('/')
 app.listen(PORT,()=>{
